df2e1866d71b63cf9848fa723b27100e436be8c8
saori-eth <d84da@proton.me>
2026-02-23 12:48:56 -0600

    revert npc.js

--- docs/scripting/examples/npc.js
+++ docs/scripting/examples/npc.js
@@ -1,19 +1,18 @@
-export default (world, app, fetch, props, setTimeout) => {
-  app.configure([
+app.configure([
     {
       key: 'name',
       type: 'text',
       label: 'Name',
-      initial: 'NPC',
-    },
+      initial: 'NPC'
+    }
   ])
-
+  
   const SEND_RATE = 0.33
   const MAX_DISTANCE = 10
   const num = prng(1)
-
+  
   app.get('Block').visible = false
-
+  
   const emotes = [
     app.asset('./assets/walk.glb'),
     app.asset('./assets/run.glb'),
@@ -26,13 +25,13 @@ export default (world, app, fetch, props, setTimeout) => {
   const walkEmoteIdx = 0
   const runEmoteIdx = 1
   const customEmoteIndices = [2, 3, 4, 5, 6]
-
+  
   if (world.isServer) {
     const state = app.state
-
+  
     state.name = app.config.name || 'NPC'
     state.vrmUrl = app.asset('./assets/avatar.vrm')
-
+  
     const ctrl = app.create('controller')
     ctrl.position.copy(app.position)
     world.add(ctrl)
@@ -102,7 +101,13 @@ export default (world, app, fetch, props, setTimeout) => {
       lastSend += delta
       if (lastSend > SEND_RATE) {
         lastSend = 0
-        app.send('change', [state.px, state.py, state.pz, state.ry, state.e])
+        app.send('change', [
+          state.px,
+          state.py,
+          state.pz,
+          state.ry,
+          state.e
+        ])
       }
     })
     state.px = ctrl.position.x
@@ -113,7 +118,7 @@ export default (world, app, fetch, props, setTimeout) => {
     state.ready = true
     app.send('init', state)
   }
-
+  
   if (world.isClient) {
     if (app.state.ready) {
       init(app.state)
@@ -124,10 +129,10 @@ export default (world, app, fetch, props, setTimeout) => {
       const root = app.create('group')
       root.position.set(state.px, state.py, state.pz)
       const avatar = app.create('avatar', {
-        src: state.vrmUrl,
+        src: state.vrmUrl
       })
       const nametag = app.create('nametag', {
-        label: state.name,
+        label: state.name
       })
       avatar.rotation.y = state.ry
       avatar.onLoad = () => {
@@ -147,4 +152,5 @@ export default (world, app, fetch, props, setTimeout) => {
       })
     }
   }
-}
+  
+  
